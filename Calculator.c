6a4f5695caad25998b8ac50817ea413e29fac499
Mohamed Gamal Aly <mohamedjamalali.1997@gmail.com>
2022-08-21 00:30:58 +0200

    adding functions definetions

--- Calculator.c
+++ Calculator.c
@@ -1,6 +1,6 @@
 typdef enum numbers
 {
-		ZERO,
+		ZERO=0,
 		ONE,
 		TWO,
 		THREE,
@@ -10,12 +10,15 @@ typdef enum numbers
 		SEVEN,
 		EIGHT,
 		NINE,
-		TEN
-	
-	
+		TEN	
 }
 
 
+
+int Add_Two_Numbers(int First_Num,int Second_Num);
+int Subtract_Two_Numbers(int First_Num,int Second_Num);
+int Multiply_Two_Numbers(int First_Num,int Second_Num);
+int Divide_Two_Numbers(int First_Num,int Second_Num);
 
 
 
@@ -25,4 +28,44 @@ int main ()
 	
 	
 	return 0;
+}
+
+
+
+
+int Add_Two_Numbers(int First_Num,int Second_Num)
+{
+	
+	int Result =0;
+	Result = First_Num+Second_Num;
+	return Result;
+}
+int Subtract_Two_Numbers(int First_Num,int Second_Num)
+{
+	
+	int Result =0;
+	Result = First_Num-Second_Num;
+	return Result;
+	
+	
+}
+
+int Multiply_Two_Numbers(int First_Num,int Second_Num)
+{
+	
+	
+	
+	int Result =0;
+	Result = First_Num*Second_Num;
+	return Result;
+}
+int Divide_Two_Numbers(int First_Num,int Second_Num)
+{
+	
+	
+	int Result =0;
+	Result = First_Num/Second_Num;
+	return Result;
+	
+	
 }
