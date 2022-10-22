ae1e25d00c916e01bf8f89b08df3b3974198cf36
Mohamed Gamal Aly <mohamedjamalali.1997@gmail.com>
2022-10-22 12:46:21 +0200

    removing the prototypes and enums from the Calculator.c to Calculator.h

--- Calculator.c
+++ Calculator.c
@@ -1,42 +1,7 @@
-typdef enum numbers
-{
-		ZERO=0,
-		ONE,
-		TWO,
-		THREE,
-		FOUR,
-		FIVE,
-		SIX,
-		SEVEN,
-		EIGHT,
-		NINE,
-		TEN	
-}
 
 
 
-int Add_Two_Numbers(int First_Num,int Second_Num);
-int Subtract_Two_Numbers(int First_Num,int Second_Num);
-int Multiply_Two_Numbers(int First_Num,int Second_Num);
-int Divide_Two_Numbers(int First_Num,int Second_Num);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#include "Calculator.h"
 
 
 
